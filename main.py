70b30eda8b654fd5ae084154af3784d9a3864699
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-05-07 22:20:14 +0800

    Removed first and second num in __init__. Renamed the first method to 'calculate' where all operations will be under. Initialized history to make the program remember previous operations. Used split() to split the equation into 3 parts and create a check using try and except to confirm that the input is valid.

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 class SimpleCalculator:
-    def __init__(self, first_num, second_num):
-        self.first_num = first_num
-        self.second_num = second_num
+    def __init__(self):
+        self.history = []
 
-    def addition(self):
-        
+    def calculate(self):
+        print("-------Simple Calculator-------")
+        print("Enter an operation (i.e., 3 * 3 or 1 + 1, etc.)")
+        print('Enter "e" to exit.')
+
+        while True:
+            equation = input("- ").strip()
+
+            if equation == 'e'  and equation == 'E':
+                print("Exiting program...")
+                break
+
+            sliced = equation.split()
+            try:
+                first_num = float(equation[0])
+                operation = equation[1]
+                second_num = float(equation[2])
+            except ValueError:
+                print("\nError: Invalid input. Please enter valid numbers.")
+                continue
