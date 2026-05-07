1846ce5a26d22a6404ad42bb9d9adc60d3aeb6b2
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-05-07 20:26:39 +0800

    Created a class, initialized with self, first, and second nums, and created a function for the addition of these numbers

---
+++ main.py
@@ -0,0 +1,7 @@
+class SimpleCalculator:
+    def __init__(self, first_num, second_num):
+        self.first_num = first_num
+        self.second_num = second_num
+
+    def addition(self):
+        
